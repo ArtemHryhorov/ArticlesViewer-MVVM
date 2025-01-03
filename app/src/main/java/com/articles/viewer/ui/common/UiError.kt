08551792e9f0bd7e7312572d479f3ecddcec940b
ArtemHryhorovGeniusee <a.hryhorov@geniusee.com>
2025-01-04 00:50:26 +0200

    Display error state

--- app/src/main/java/com/articles/viewer/ui/common/UiError.kt
+++ app/src/main/java/com/articles/viewer/ui/common/UiError.kt
@@ -1,18 +1,23 @@
 package com.articles.viewer.ui.common
 
 import com.articles.viewer.core.exceptions.ServerUnavailableException
+import java.net.UnknownHostException
 
 data class UiError(
     val title: String,
     val description: String? = null,
 ) {
 
-    // TODO("Localisation")
+    // TODO("Add localisation by passing context")
     companion object {
         fun fromThrowable(error: Throwable) = when (error) {
             is ServerUnavailableException -> UiError(
                 title = "Service is unavailable",
                 description = "Try to connect with support team",
+            )
+            is UnknownHostException -> UiError(
+                title = "Failed to load",
+                description = "Check your Internet connection and try again later",
             )
             else -> UiError(title = "Something went wrong")
         }
